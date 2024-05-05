893108cfc2bf2c67f1d3c660ff1ae041e5cfc576
marcostenoriodev <marcosvtenoriodev@gmail.com>
2024-05-05 19:41:39 -0300

    add homepage and routing config in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
 import { ToastContainer } from "react-toastify";
-import { useT } from "./assets/i18n";
 import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+
+function Layout({ children }: any) {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+}
 
 const App = () => {
   /*   const { showToast } = useToastContext();
-   */ const t = useT();
+    const t = useT(); */
 
   return (
     <div className="App">
       <ToastContainer />
-      <h1>{t("application.helloWorld.title")}</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <Home />
+              </Layout>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };

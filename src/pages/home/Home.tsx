a0e83f2d90e86280f59d806f88f5bffad72e7205
marcostenoriodev <marcosvtenoriodev@gmail.com>
2024-05-12 16:17:27 -0300

    add Carousel in homepage with mock images

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,84 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  CarouselApi,
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselPrevious,
+  CarouselNext,
+} from "@/components/ui/carousel";
+import { useState, useEffect } from "react";
+import banner1 from "@/assets/mockImages/banner1.jpeg";
+import banner2 from "@/assets/mockImages/banner2.jpg";
+import Autoplay from "embla-carousel-autoplay";
 
-export default Home
+const Home = () => {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+  const [imagesBanner, setImagesBanner] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+    setCurrent(api.selectedScrollSnap() + 1);
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+    setImagesBanner([banner1, banner2, banner1, banner2]);
+    setCount(imagesBanner.length);
+  }, [api]);
+
+  return (
+    <div className="flex flex-col items-center justify-center mt-8">
+      <Carousel
+        setApi={setApi}
+        className="w-full sm:w-10/12 md:w-8/12 lg:w-7/12 max-w-[1200px]"
+        opts={{
+          loop: true,
+        }}
+        plugins={[
+          Autoplay({
+            delay: 3000,
+          }),
+        ]}
+      >
+        <CarouselContent>
+          {imagesBanner.map((image, index) => {
+            return (
+              <CarouselItem key={index}>
+                <Card>
+                  <CardContent className="flex aspect-video items-center justify-center p-0">
+                    <img
+                      src={image}
+                      alt="imagem do banner"
+                      className="object-cover w-full h-full rounded-xl"
+                    />
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+      <div className="py-2 text-center text-sm text-muted-foreground flex items-center justify-center">
+        <div className="flex gap-[4px]">
+          {Array.from({ length: count }).map((_, index) => {
+            return (
+              <div key={index}
+                className={`w-2 h-2 rounded-full ${
+                  index + 1 === current ? "bg-black" : "bg-gray-300"
+                }`}
+              ></div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

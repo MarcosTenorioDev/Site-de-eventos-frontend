a7ea1e1bda46182eca912b1dd38b5b4c9cbf47ad
carlossilvacosta <carlossergiosilvacosta@gmail.com>
2024-05-24 09:27:58 -0300

    fix navbar button text

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -88,7 +88,7 @@ const Navbar = () => {
 									setIsMenuOpen(false);
 								}}
 							>
-								{t("application.components.navbar.myEvents")}
+								{t("application.components.navbar.createEvent")}
 							</Link>
 							<Link
 								className="h-9 px-4 py-2 bg-primary text-primary-foreground shadow hover:opacity-90 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"

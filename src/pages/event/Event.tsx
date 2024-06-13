e3925b0b95bb7e066ac1cf10fa61c12fc644936a
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2024-06-13 10:41:15 -0300

    Ajustando a exibição do backgroundImage utilizando encodeURI

--- src/pages/event/Event.tsx
+++ src/pages/event/Event.tsx
@@ -225,7 +225,7 @@ const Event = () => {
 					<div
 						className="absolute -inset-5 bg-cover bg-no-repeat blur-[10px] -z-10 mt-[70px] bg-[100% 100%] max-h-[700px]"
 						style={{
-							backgroundImage: `url(${event.assets[0]?.url})`,
+							backgroundImage: `url(${encodeURI(event.assets[0]?.url)})`,
 						}}
 					/>
 					<div className="flex flex-col items-center justify-center w-full">
